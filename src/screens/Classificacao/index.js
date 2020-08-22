ed425f676d23efc09d75211a2193a2f0ecbce4e8
igor-resende-ifood <igor.resende@ifood.com.br>
2020-08-22 11:50:22 -0300

    fix botao voltar

--- src/screens/Classificacao/index.js
+++ src/screens/Classificacao/index.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Dimensions, TouchableHighlight, Button, SafeAreaView, RefreshControl, DatePickerAndroid, ToastAndroid } from 'react-native';
+import { Dimensions, TouchableHighlight, Button, SafeAreaView, RefreshControl, DatePickerAndroid, ToastAndroid, BackHandler } from 'react-native';
 import { StyleSheet, Image, View, Text } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 import axios from 'axios';
 export const loaderRef = React.createRef();
 import PTRView from 'react-native-pull-to-refresh';
 import Modal from 'react-native-modal';
+import Toast from 'react-native-root-toast';
 
 
 
 export default function Example() {
   var header = "Classificação Rodada: "
   var rodada_atual = 0
+  var posicoes = ["Error", "G", "L", "Z", "M", "A", "T"]
+  var clubes = { 373: "https://s.glbimg.com/es/sde/f/organizacoes/2020/07/02/atletico-go-2020-45.png", 276: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/sao_paulo_45x45.png", 356: "https://s.glbimg.com/es/sde/f/organizacoes/2018/06/10/fortaleza-ec-45px.png", 292: "https://s.glbimg.com/es/sde/f/organizacoes/2015/07/21/sport45.png", 277: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/santos_45x45.png", 293: "https://s.glbimg.com/es/sde/f/organizacoes/2019/09/09/Athletico-PR-45x45.png", 263: "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/04/botafogo-45.png", 262: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/09/Flamengo-45.png", 354: "https://s.glbimg.com/es/sde/f/organizacoes/2019/10/10/ceara-45x45.png", 267: "https://s.glbimg.com/es/sde/f/organizacoes/2016/07/29/Vasco-45.png", 266: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/fluminense_45x45.png", 265: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/bahia_45x45.png", 264: "https://s.glbimg.com/es/sde/f/organizacoes/2019/09/30/Corinthians_45.png", 280: "https://s.glbimg.com/es/sde/f/organizacoes/2020/01/01/45.png", 294: "https://s.glbimg.com/es/sde/f/organizacoes/2017/03/29/coritiba45.png", 282: "https://s.glbimg.com/es/sde/f/organizacoes/2017/11/23/Atletico-Mineiro-escudo45px.png", 285: "https://s.glbimg.com/es/sde/f/organizacoes/2016/05/03/inter45.png", 284: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/gremio_45x45.png", 290: "https://s.glbimg.com/es/sde/f/organizacoes/2019/05/01/Goias_45px.png", 275: "https://s.glbimg.com/es/sde/f/organizacoes/2014/04/14/palmeiras_45x45.png", }
+
+
+
   const [items, setItems] = React.useState([
     { name: "", backgroundColor: "#008000", textColor: "#008000", pointsColor: "#008000", id: 0, pts: 10001 },
     { name: header + rodada_atual, backgroundColor: "#008000", textColor: "#fff", pointsColor: "#008000", id: 0, pts: 10000 },
 
-    { name: "5prastantas F.C.", slug: require("../../assets/escudos/5prastantas-f-c.png"), backgroundColor: "#808080", textColor: "#ffffff", pointsColor: "#ffffff", id: 13945827, maiorPontuacao: 68, pontuacaoUltimaRodada: 34, pts: 0, atletas: [70800, 86485, 38750, 88312, 98974, 82455, 78086, 72772, 80853, 83257, 8177, 892566], pegouAtletas: false, numAtletasPontuando: 0, capitaoId: 0, distrital: false, isModalVisible: false },
+    { name: "5prastantas F.C.", slug: require("../../assets/escudos/5prastantas-f-c.png"), backgroundColor: "#808080", textColor: "#ffffff", pointsColor: "#ffffff", id: 13945827, maiorPontuacao: 68, pontuacaoUltimaRodada: 34, pts: 0, atletas: [], pegouAtletas: false, numAtletasPontuando: 0, capitaoId: 0, distrital: false, isModalVisible: false },
     { name: "AA AZULAO DO SUL", slug: require("../../assets/escudos/aa-azulao-do-sul.png"), backgroundColor: "#ff241d", textColor: "#063780", pointsColor: "#063780", id: 6717458, maiorPontuacao: 35, pontuacaoUltimaRodada: 35, pts: 0, atletas: [], pegouAtletas: false, numAtletasPontuando: 0, capitaoId: 0, distrital: false, isModalVisible: false },
     { name: "Akuma Sport Club", slug: require("../../assets/escudos/akuma-sport-club.png"), backgroundColor: "#ffffff", textColor: "#000000", pointsColor: "#000000", id: 483335, maiorPontuacao: 63, pontuacaoUltimaRodada: 38, pts: 0, atletas: [], pegouAtletas: false, numAtletasPontuando: 0, capitaoId: 0, distrital: false, isModalVisible: false },
     { name: "Atleticado", slug: require("../../assets/escudos/atleticado.png"), backgroundColor: "#4d004d", textColor: "#0066ff", pointsColor: "#0066ff", id: 661306, maiorPontuacao: 65, pontuacaoUltimaRodada: 21, pts: 0, atletas: [], pegouAtletas: false, numAtletasPontuando: 0, capitaoId: 0, distrital: false, isModalVisible: false },
@@ -59,17 +65,52 @@ export default function Example() {
     // { name: "", backgroundColor: "#008000", textColor: "#008000", pointsColor: "#008000", id: 0, pts: -101 },        
   ]);
 
+  //   let toast = Toast.show('This is a message', {
+  //     duration: 50000,
+  //     position: Toast.positions.CENTER,
+  //     shadow: true,
+  //     animation: true,
+  //     hideOnPress: true,
+  //     delay: 0,
+  //     onShow: () => {
+  //         // calls on toast\`s appear animation start
+  //         console.log("começou")
+  //     },
+  //     onShown: () => {
+  //         // calls on toast\`s appear animation end.
+  //     },
+  //     onHide: () => {
+  //         // calls on toast\`s hide animation start.
+  //         console.log("terminou")
+  //     },
+  //     onHidden: () => {
+  //         // calls on toast\`s hide animation end.
+  //     }
+  // });
+
   var status = 1
+  var firstTime = true
+
   async function refreshData() {
     // console.log("------entrou aqui---------------")
     // axios.get("https://cartolaigor-pjzxn.hoverfly.io/mercado/status")    
     await axios.get("https://api.cartolafc.globo.com/mercado/status")
       .then(res => {
         status = res.data.status_mercado;
-        rodada_atual = res.data.rodada_atual
+        if (status == 2) {
+          rodada_atual = res.data.rodada_atual
+        }
+        else {
+          rodada_atual = res.data.rodada_atual - 1
+        }
         items[1].name = header + rodada_atual
         setItems([...items], items);
       });
+
+    if (firstTime) {
+      await getAtletas();
+      firstTime = false;
+    }
 
     if (status == 2) {
       // console.log("------entrou aqui2---------------")
@@ -95,11 +136,13 @@ export default function Example() {
                   .then(res => {
                     // console.log(res.status)
                     if (res.status == 200) {
-                      res.data.atletas.forEach(atleta => {
-                        element.atletas.push(atleta.atleta_id)
-                      })
+                      // res.data.atletas.forEach(atleta => {
+                      element.atletas = res.data.atletas;
+                      // })
+                      // console.log(element.atletas)
                       element.pegouAtletas = true
                       element.capitaoId = res.data.capitao_id;
+                      element.atletas.sort((a, b) => (a.posicao_id >= b.posicao_id) ? 1 : -1)
                       setItems([...items], items);
                     }
                   })
@@ -113,9 +156,18 @@ export default function Example() {
                 element.numAtletasPontuando = 0;
                 element.atletas.forEach(atleta => {
                   try {
-                    element.pts += atletasPontuando[atleta].pontuacao;
+                    element.pts += atletasPontuando[atleta.atleta_id].pontuacao;
+                    atleta.pontos_num = atletasPontuando[atleta.atleta_id].pontuacao;
+                    atleta.status_id = 1 // 1 = pontuando, 0 = nao pontuando
                     element.numAtletasPontuando += 1;
-                  } catch { }
+                    // console.log(!atleta.apelido.contains("(c)"))
+                    // if (element.capitaoId == atleta.atleta_id && !atleta.apelido.includes("(c)")) {
+                    //   atleta.apelido = atleta.apelido + " (c)"
+                    // }
+                  } catch {
+                    atleta.pontos_num = 0;
+                    atleta.status_id = 0 // 1 = pontuando, 0 = nao pontuando
+                  }
                 })
 
                 try {
@@ -131,7 +183,43 @@ export default function Example() {
           }
         }
       });
+    } else {
+      items.forEach(element => {
+
+        items.sort((a, b) => (a.pts <= b.pts) ? 1 : -1)
+        if (element.id != 0) {
+          try {
+            setTimeout(function () {
+              if (!element.pegouAtletas) {
+                axios.get("https://api.cartolafc.globo.com/time/id/" + element.id + "/" + rodada_atual)
+                  .then(res => {
+                    if (res.status == 200) {
+                      element.atletas = res.data.atletas;
+                      element.pts = parseInt(res.data.pontos);
+                      element.pegouAtletas = true;
+                      element.capitaoId = res.data.capitao_id;
+                      element.atletas.sort((a, b) => (a.posicao_id >= b.posicao_id) ? 1 : -1)
+                      element.numAtletasPontuando = 12
+                    }
+                  })
+
+
+              }
+              // setItems([...items], items);
+              // console.log(atleta.clube_id)
+
+
+            }, 500);
+          } catch { }
+          items.sort((a, b) => (a.pts <= b.pts) ? 1 : -1)
+          setItems([...items], items);
+        }
+      });
     }
+
+
+
+
     // else {
     //   items.forEach(equipe => {
     //     if (equipe.id != 0) {
@@ -147,25 +235,27 @@ export default function Example() {
     //     }
     //   })
     // }
+
+
+
+
+
+
   }
-  // useState(() => {
-  //   refreshData();
-  // }, [])
 
-
-  useEffect(() => {
-    getAtletas()
-    refreshData()
-  }, []);
+  global.jsonAtletas = null
 
   function getAtletas() {
     axios.get("https://api.cartolafc.globo.com/atletas/mercado")
       .then(responseAtletas => {
-        const jsonAtletas = responseAtletas.data
+        global.jsonAtletas = responseAtletas.data.atletas
         // console.log(atletasPontuando)
       });
   }
 
+  useEffect(() => {
+    refreshData();
+  }, []);
 
   // const [isModalVisible, setModalVisible] = useState(false);
 
@@ -182,6 +272,7 @@ export default function Example() {
       });
     }} >
       <View style={{ flex: 1 }}>
+
 
         <FlatGrid
           itemDimension={Dimensions.get('window').width}
@@ -204,7 +295,7 @@ export default function Example() {
 
                 {/* Nome dos times */}
                 <View style={[styles.itemContainer, { borderColor: item.pointsColor, marginHorizontal: Dimensions.get('window').width * 0.003, backgroundColor: item.backgroundColor, width: Dimensions.get('window').width * 0.74 }]}>
-                  <Text style={[styles.itemName, { color: item.textColor }]}>{item.name}</Text>
+                  <Text adjustsFontSizeToFit={true} style={[styles.itemName, { color: item.textColor }]}>{item.name}</Text>
                 </View>
 
                 {/* Escudo dos times */}
@@ -212,20 +303,73 @@ export default function Example() {
 
                 {/* Pontuacao dos times */}
                 <View style={[styles.itemContainer, { borderColor: item.pointsColor, shadowColor: item.backgroundColor, marginHorizontal: Dimensions.get('window').width * 0.003, backgroundColor: item.backgroundColor, width: Dimensions.get('window').width * 0.14 }]}>
-                  <Text style={[styles.itemName, { color: item.pointsColor }]}>{item.pts}</Text>
+                  <Text adjustsFontSizeToFit={true} style={[styles.itemName, { color: item.pointsColor }]}>{item.pts}</Text>
                 </View>
 
                 {/* Num de atletas pontuando */}
                 <View style={[item.pts < 1000 ? { borderColor: item.backgroundColor, borderWidth: 1, justifyContent: 'center', alignItems: "center", borderRadius: 100, position: 'absolute', marginVertical: Dimensions.get('window').height * 0.02, marginHorizontal: Dimensions.get('window').width * 0.82, backgroundColor: item.textColor, width: Dimensions.get('window').width * 0.05, height: Dimensions.get('window').width * 0.05 } : {}]}>
-                  <Text style={[styles.itemName, { fontSize: 10, color: item.backgroundColor }]}>{item.numAtletasPontuando}</Text>
+                  <Text adjustsFontSizeToFit={true} style={[styles.itemName, { fontSize: 10, color: item.backgroundColor }]}>{item.numAtletasPontuando}</Text>
                 </View>
 
 
-                <Modal isVisible={item.isModalVisible} coverScreen={true} backdropColor={item.pointsColor} backdropOpacity={0.94} >
-                  <View style={{ flex: 1, alignItems: 'center', marginVertical: Dimensions.get('window').width * 0.2 }} >
-                    {console.log(jsonAtletas.)}
-                    {item.id > 0 ? (item.atletas.map(x => <Text style={[styles.itemName, { fontSize: 20, color: item.backgroundColor, width: Dimensions.get('window').width * 0.9, marginVertical: Dimensions.get('window').width * 0.01 }]}>{x}</Text>)) : {}}
-                    <Button title={item.name} color={item.backgroundColor} onPress={() => toggleModal(item)} />
+                <Modal isVisible={item.isModalVisible} coverScreen={true} backdropColor="#008000" backdropOpacity={0.94} onRequestClose={() => { toggleModal(item) }}>
+                  <View style={{ flex: 1, alignItems: 'center', marginVertical: Dimensions.get('window').width * 0.1 }} >
+
+                    {/* header time */}
+                    <View style={[styles.itemContainer, { borderWidth: 1, marginHorizontal: Dimensions.get('window').width * 0.003, backgroundColor: item.textColor, width: Dimensions.get('window').width * 0.97 }]}>
+                      <Text adjustsFontSizeToFit={true} style={[styles.itemName, { color: item.backgroundColor }]}>{item.name}</Text>
+                    </View>
+
+                    {item.id > 0 ? (item.atletas.map(x =>
+                      // clubes
+                      <View style={{ flexDirection: 'row' }}>
+                        <View style={[styles.itemContainer, { borderWidth: 1, borderColor: item.textColor, backgroundColor: item.backgroundColor, width: Dimensions.get('window').width * 0.08 }]}>
+                          {/* <Text adjustsFontSizeToFit={true} style={[styles.itemName, { fontSize: 25, color: item.pointsColor }]}>{posicoes[x.posicao_id]}</Text> */}
+                          <Image source={{ uri: clubes[x.clube_id] }} style={{ marginVertical: Dimensions.get('window').height * 0.005, marginHorizontal: Dimensions.get('window').width * 0.06, height: Dimensions.get('window').width * 0.05, width: Dimensions.get('window').width * 0.05, resizeMode: 'stretch' }} />
+                        </View>
+
+                        {/*  Posicoes Jogadores */}
+                        <View style={[styles.itemContainer, { borderWidth: 1, borderColor: item.textColor, backgroundColor: item.backgroundColor, width: Dimensions.get('window').width * 0.08 }]}>
+                          <Text adjustsFontSizeToFit={true} style={[styles.itemName, { fontSize: 25, color: item.pointsColor }]}>{posicoes[x.posicao_id]}</Text>
+                        </View>
+
+                        {/* Nome Jogadores */}
+                        <View style={[item.capitaoId == x.atleta_id ? { backgroundColor: item.textColor, borderColor: item.backgroundColor } : { backgroundColor: item.backgroundColor, borderColor: item.textColor }, styles.itemContainer, { borderWidth: 1, marginHorizontal: Dimensions.get('window').width * 0.003, width: Dimensions.get('window').width * 0.55 }]}>
+                          <Text adjustsFontSizeToFit={true} style={[item.capitaoId == x.atleta_id ? { color: item.backgroundColor } : { color: item.textColor }, styles.itemName]}>{item.capitaoId == x.atleta_id ? x.apelido + ' (c)' : x.apelido}</Text>
+                        </View>
+
+                        {/* {console.log()} */}
+                        {/* Pontuacao dos jogadores */}
+                        <View style={[styles.itemContainer, { borderWidth: 1, borderColor: item.textColor, shadowColor: item.backgroundColor, marginHorizontal: Dimensions.get('window').width * 0.003, backgroundColor: item.backgroundColor, width: Dimensions.get('window').width * 0.24 }]}>
+                          <Text adjustsFontSizeToFit={true} numberOfLines={1} style={[styles.itemName, { color: item.pointsColor }]}>{x.status_id != 0 ? item.capitaoId == x.atleta_id ? '(' + x.pontos_num + ' x2) ' + x.pontos_num * 2 : x.pontos_num : '-'}</Text>
+                        </View>
+
+                        {/* {console.log(x.clube_id)} */}
+                        {/* Escudo dos times */}
+
+
+
+                      </View>)) : {}}
+
+                    <View style={{ flexDirection: 'row' }}>
+                      {/* TOTAL: */}
+                      <View style={[{ backgroundColor: item.textColor, borderColor: item.backgroundColor }, styles.itemContainer, { borderWidth: 1, marginHorizontal: Dimensions.get('window').width * 0.003, width: Dimensions.get('window').width * 0.72 }]}>
+                        <Text adjustsFontSizeToFit={true} style={[{ color: item.backgroundColor }, styles.itemName]}>TOTAL:</Text>
+                      </View>
+
+                      {/* ponntuacao total com capitao */}
+                      <View style={[styles.itemContainer, { borderWidth: 1, borderColor: item.backgroundColor, marginHorizontal: Dimensions.get('window').width * 0.003, backgroundColor: item.textColor, width: Dimensions.get('window').width * 0.24 }]}>
+                        <Text adjustsFontSizeToFit={true} numberOfLines={1} style={[styles.itemName, { color: item.backgroundColor }]}>{item.pts}</Text>
+                      </View>
+                    </View>
+
+                    {/* botao voltar */}
+                    <TouchableHighlight onPress={() => toggleModal(item)} underlayColor={'#008000'} >
+                      <View style={[{ position: "relative" }, { borderWidth: 2,justifyContent: 'center' ,alignItems: 'center', fontSize: 20, height: Dimensions.get('window').height * 0.06, width: Dimensions.get('window').width * 0.95, marginVertical: '5%', marginHorizontal: '-17.5%' }]}>
+                        <Text adjustsFontSizeToFit={true} numberOfLines={1} style={[{fontSize:20}]}>Voltar</Text>
+                      </View>
+                    </TouchableHighlight>
+                    
                   </View>
                 </Modal>
 
